2793a2a7e06da99996d7e59605541dce69b7db8c
ChiuYuChun <qwe3322568@gmail.com>
2022-07-31 00:01:37 +0800

    testv1.0

---
+++ tailwind.config.js
@@ -0,0 +1,84 @@
+module.exports = {
+  content: ["./app/**/*.{html,ejs}"],
+  theme: {
+    container:{
+      center:true,
+      padding:'12px',
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm-max': {'max': '720px'},
+
+      // // => @media (max-width: 639px) { ... }
+      'sm': {'min': '1320px'},
+      'sm-md':{'min':'720px'},
+      // => @media (max-width: 639px) { ... }
+      'sm-mini':{'max':'576px'},
+    }, spacing: {
+      px: '1px',
+      0: '0',
+      0.5: '0.125rem',
+      1: '0.25rem',
+      1.5: '0.375rem',
+      2: '0.5rem',
+      2.5: '0.625rem',
+      3: '0.75rem',
+      3.5: '0.875rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      9: '2.25rem',
+      10: '2.5rem',
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      20: '5rem',
+      24: '6rem',
+      28: '7rem',
+      32: '8rem',
+      36: '9rem',
+      40: '10rem',
+      44: '11rem',
+      48: '12rem',
+      52: '13rem',
+      56: '14rem',
+      60: '15rem',
+      64: '16rem',
+      72: '18rem',
+      80: '20rem',
+      96: '24rem',
+    },
+    extend: {
+      colors:{
+      'maroon':{
+        'light':'#FBF2F2',
+        DEFAULT:'#AA0601',
+        'dark':'#650300',
+      },  
+      // backgroundImage: {
+      //   'home-header': "url('./assets/images/home-header.png')",
+      // }
+    }
+  
+  
+  },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
